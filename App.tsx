84ceb1a89898dfac0089b22e8ae2a972974be4e3
Daniel <daniel@Daniels-iMac-Pro.local>
2021-04-25 13:54:03 -0300

    Projeto PlantManager NLW5

--- App.tsx
+++ App.tsx
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React, { useEffect } from 'react';
+import * as Notifications from 'expo-notifications';
+
+import Routes from './src/routes';
+import { PlantProps } from './src/libs/storage';
+
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold,
+} from '@expo-google-fonts/jost';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(
+      async (notification) => {
+        const data = notification.request.content.data.plant as PlantProps;
+        console.log(data);
+      }
+    );
+    return () => subscription.remove();
+
+    // VISUALIZAR TODAS NOTIFICAÇÕES NO CONSOLE
+    // async function notificationsAll() {
+    //   const data = await Notifications.getAllScheduledNotificationsAsync()
+    //   console.log('############### NOTIFICAÇÕES AGENDADAS ######################')
+    //   console.log(data)
+    // }
+    // notificationsAll()
+
+    // DELETAR TODAS AS NOTIFICAÇÕES
+    // async function notificationDelete() {
+    //   await Notifications.cancelAllScheduledNotificationsAsync();
+    //   const data = await Notifications.getAllScheduledNotificationsAsync()
+    //   console.log('############### NOTIFICAÇÕES AGENDADAS ######################')
+    //   console.log(data)
+    // }
+    // notificationDelete()
+  }, []);
+
+  if (!fontsLoaded) return <AppLoading />;
+
+  return <Routes />;
+}
